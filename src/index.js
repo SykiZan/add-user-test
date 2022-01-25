31bb6b3849b3e9ad41143723b5de7fa03169bc74
SykiZan <newmlsz340@gmail.com>
2022-01-25 13:15:10 +0200

    submitted

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import usersStore from "./store/store";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={usersStore}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
